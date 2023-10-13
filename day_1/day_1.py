a5c31ecacf369a4d91460ec6deb1a5fb0b41af35
Ajmal Quraishi <ajmalquraishi98@gmail.com>
2023-10-13 23:26:27 +0500

    Start 13-10-23

---
+++ day_1/day_1.py
@@ -0,0 +1,30 @@
+# Lists vs. Tuples:
+
+# Lists are mutable, meaning their elements can be changed after creation, whereas tuples are immutable, and their elements cannot be changed.
+# Lists are defined using square brackets
+list1=[1,"Ajmal","Karachi"],
+#Lists are mutable
+# Tuples are defined using parentheses
+my_tuple = (1, 2, 3),
+# Tuples are immutable
+#Lists are generally used for collections of similar items that may need to be modified, while tuples are used for collections of heterogeneous items that should remain constant.
+#Lists vs. Sets:
+#Lists allow duplicate elements and maintain order, while sets do not allow duplicates and do not guarantee order.
+list1=[1,"Ajmal","Karachi"],
+#Sets are immutable
+set1 ={1,2,3},
+#Lists vs. Dictionaries:
+#Lists are ordered collections of items accessed by their index (integer positions), while dictionaries are collections of key-value pairs.
+list1=[1,"Ajmal","Karachi"],
+#Dictionaries are mutable
+dic1={'name':'Ajmal','age':25,}
+#Dictionaries have key-value pairs.
+
+# Sets vs. Dictionaries:
+# Sets are collections of unique elements without key-value pairs, while dictionaries are collections of key-value pairs.
+set1 ={1,2,3},
+
+dic1={'name':'Ajmal','age':25,}
+# Dictionaries are unordered collections of key-value pairs.
+print(dic1)
+
---
+++ day_1/day_2.py
@@ -0,0 +1,21 @@
+# while loop
+# a = 1;
+# while a <=100:
+#      print(a)
+#      a = a + 1
+# # for loop
+# for i in range(4):
+#     print(i)
+    
+for i in 'Ajmal':
+        if i=='j':
+            continue
+        print(i);
+for i in 'Ajmal':
+        if i=='j':
+            continue
+        print(i);
+for i in 'Ajmal':
+        if i=='j':
+            continue
+        print(i);
