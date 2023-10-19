23417f78ddd723cdec0a11ec9d3a2c0e70ab79a1
Ajmal Quraishi <ajmalquraishi98@gmail.com>
2023-10-20 00:29:51 +0500

    Add some

--- lamda_function.py
+++ lamda_function.py
@@ -12,6 +12,27 @@ second_function('Quraishi');
 def square(num):
     return num*num;
 
+# input1 = int(input("Enter a number: "));
+# result = square(input1);
+# print(result);
+# #recursion function (a function that calls itself) and (repeating a task)
+# def factorial(n):
+#     if n==0:
+#         return 1;
+#     return n*factorial(n-1);
+
+# input1 = int(input("Enter a number: "));
+# result = factorial(input1);
+# print(result);
+#lambda function (anonymous function)and (one line of code)
+#old def
+def square(num):
+    return num*num;
+square = lambda num: num*num;
 input1 = int(input("Enter a number: "));
 result = square(input1);
 print(result);
+def add(a, b):
+    return a + b;
+x = lambda a, b: a + b;
+print(x(5, 6));
